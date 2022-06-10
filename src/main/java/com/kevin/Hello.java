fa8bcf55829aa3af723660399ebffcd00291d384
KevinDaPlayer <325881@gmail.com>
2022-06-10 15:03:24 +0800

    Hello world with class Person

--- src/main/java/com/kevin/Hello.java
+++ src/main/java/com/kevin/Hello.java
@@ -1,4 +1,9 @@
 package com.kevin;
 
 public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello");
+        Person p = new Person();
+        p.greeting();
+    }
 }
---
+++ src/main/java/com/kevin/Person.java
@@ -0,0 +1,7 @@
+package com.kevin;
+
+public class Person {
+    public void greeting () {
+        System.out.println("Hello");
+    }
+}
